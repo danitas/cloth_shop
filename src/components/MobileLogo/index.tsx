f861bc84cac560b350c68e84317fabe5f7f508b0
K.Prikhodko <prikhodko99@gmail.com>
2025-01-04 18:13:17 +0100

    change encoding

--- src/components/MobileLogo/index.tsx
+++ src/components/MobileLogo/index.tsx
@@ -3,7 +3,7 @@ import logoXS from '@assets/images/logo_xs.svg';
 const MobileLogo = () => {
   return (
     <a href="/" title="Home">
-      <img className="h-11 w-10 sm:flex lg:hidden" src={logoXS} alt="logo" />
+      <img className="h-11 w-10 sm:flex lg:hidden" src={logoXS} alt="Logo" />
     </a>
   );
 };
