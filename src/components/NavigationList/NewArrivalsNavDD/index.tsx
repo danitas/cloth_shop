1de525815ef4d469ea30be4652a240d3a503f121
Daryna Bondarenko <daryna.bondarenko@osf.digital>
2025-01-09 19:57:24 +0100

    - Added image scale on hover

--- src/components/NavigationList/NewArrivalsNavDD/index.tsx
+++ src/components/NavigationList/NewArrivalsNavDD/index.tsx
@@ -7,23 +7,19 @@ type TImage = {
 const NewArrivalsNavDD = ({ imgSource }: TImage) => {
   return (
     <div className="children-menu-banner group flex flex-col">
-      <a className="flex flex-col" href={ROUTES.NEW_ARRIVALS}>
+      <a className="mt-4 flex flex-col" href={ROUTES.NEW_ARRIVALS}>
+        <div className="overflow-hidden">
+          <img
+            src={imgSource}
+            alt="NewArrivalsNavDD"
+            className="h-auto w-full transform transition-transform duration-1000 group-hover:scale-110"
+          />
+        </div>
         <div className="mt-4 flex justify-between">
-          <div className="overflow-hidden">
-            <img
-              src={imgSource}
-              alt="NewArrivalsNavDD"
-              className="h-auto w-full transform transition-transform duration-1000 group-hover:scale-110"
-            />
-            <div className="mt-4 flex justify-between">
-              <p className="flex text-black hover:text-black hover:underline">
-                NEW
-              </p>
-              <p className="flex text-black underline hover:text-black">
-                to go
-              </p>
-            </div>
-          </div>
+          <p className="flex text-black hover:text-black hover:underline">
+            NEW
+          </p>
+          <p className="flex text-black underline hover:text-black">to go</p>
         </div>
       </a>
     </div>
